4008d5f8eeba838905bb85c72f31ea3290f4dece
Rizkyawan028 <61112507+Rizkyawan028@users.noreply.github.com>
2020-03-30 22:05:38 +0800

    Ketik .oi

---
+++ userbot/modules/oi.py
@@ -0,0 +1,20 @@
+import asyncio
+import re
+import time
+from time import sleep
+from userbot import CMD_HELP, ZALG_LIST
+from userbot.events import register
+
+@register(outgoing=True, pattern='^.oi(?: |$)(.*)')
+async def typewriter(typew):
+	message = typew.pattern_match.group(1)
+	sleep(1)
+	await typew.edit("`Muka Gw Burik...`")
+	sleep(1)
+	await typew.edit("`Kayak Kontol`")
+# Create by myself @AkameNFS
+
+
+
+
+
